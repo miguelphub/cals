2d46aa0ea95549e1e650945f5b0d02e55e29694d
miguelnmms <148149796+miguelphub@users.noreply.github.com>
2025-05-07 21:06:36 -0600

    Quitar 5000

--- js/scotch.js
+++ js/scotch.js
@@ -1,17 +1,16 @@
 function calcularRollos() {
-    const bolsas = parseInt(document.getElementById("bolsas").value);
-    const cintaPorBolsa = parseFloat(document.getElementById("cintaPorBolsa").value);
-    const largoRollo = parseFloat(document.getElementById("largoRollo").value);
-  
-    if (isNaN(bolsas) || isNaN(cintaPorBolsa) || isNaN(largoRollo)) {
-      document.getElementById("resultado").textContent = "Agrega los datos";
-      return;
-    }
-  
-    const totalCinta = bolsas * cintaPorBolsa;
-    const rollosNecesarios = Math.ceil(totalCinta / largoRollo);
-  
-    document.getElementById("resultado").textContent =
-      `Se necesita ${totalCinta} cm de scotch. Rollos necesarios: ${rollosNecesarios}`;
+  const bolsas = parseInt(document.getElementById("bolsas").value);
+  const cintaPorBolsa = parseFloat(document.getElementById("cintaPorBolsa").value);
+  const largoRollo = 5000; // Valor fijo
+
+  if (isNaN(bolsas) || isNaN(cintaPorBolsa)) {
+    document.getElementById("resultado").textContent = "Agrega los datos";
+    return;
   }
-  
+
+  const totalCinta = bolsas * cintaPorBolsa;
+  const rollosNecesarios = Math.ceil(totalCinta / largoRollo);
+
+  document.getElementById("resultado").textContent =
+    `Se necesita ${totalCinta} cm de scotch. Rollos necesarios: ${rollosNecesarios}`;
+}
